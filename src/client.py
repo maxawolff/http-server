3476d97fbaa3170411bfb97a7fc15f35f0d03706
maxawolff <maxawolff@hotmail.com>
2017-10-23 17:08:54 -0700

    wrote server.py and tested the two functions together, only worked once

--- src/client.py
+++ src/client.py
@@ -9,7 +9,6 @@ def client(message, buffer=8):
     stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
-    message = unicode(message, "utf-8")
     client.sendall(message.encode('utf8'))
 
     buffer_length = buffer
@@ -21,4 +20,9 @@ def client(message, buffer=8):
         if len(part) < buffer_length:
             break
     client.close()
-    return reply_string
+    print(reply_string)
+
+
+if __name__ == '__main__':
+    import sys
+    client(sys.argv[1])
---
+++ src/server.py
@@ -0,0 +1,29 @@
+"""."""
+
+import socket
+
+
+def server():
+    """."""
+    server = socket.socket(socket.AF_INET,
+                           socket.SOCK_STREAM,
+                           socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5000)
+    server.bind(address)
+    server.listen(1)
+    conn, addr = server.accept()
+    message = ''
+    if conn:
+        buffer_length = 8
+        message_complete = False
+        while not message_complete:
+            part = conn.recv(buffer_length)
+            message += part.decode('utf8')
+            if len(part) < buffer_length:
+                break
+        conn.sendall(message.encode('utf8'))
+        conn.close()
+
+
+if __name__ == '__main__':
+    server()
